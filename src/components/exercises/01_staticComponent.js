d6295a84e83b50a2eb21b2da02c9f9dce2e12977
Maeliosa Leamy <59532461+red28mil@users.noreply.github.com>
2021-07-09 18:45:31 +0100

    exercise 1 completed

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,8 +3,30 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
+    <div>
       <h1>TODO</h1>
+  <table clssName="table table-bordered">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>No lectures</th>
+      <th>No practicals</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <td>DevOps</td>
+      <td>2</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Enterprise Web Dev</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+  </tbody >
+</table>
+</div>
   );
-};
-
+  }
 export default Demo;
--- src/stories/exercises/01-component.stories.js
+++ src/stories/exercises/01-component.stories.js
@@ -9,3 +9,4 @@ export default {
 export const Basic = () => {
   return <CourseModulesStatic />  ;
 };
+
