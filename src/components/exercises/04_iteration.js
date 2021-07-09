64bbf2deb7c4d26cba96a77a33291061a3e535bd
Maeliosa Leamy <59532461+red28mil@users.noreply.github.com>
2021-07-09 21:34:14 +0100

    Exercise 4 completed

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
-const Demo = () => {
+const Demo = props => {
+  const list = props.modules.map((module, index) => {
+    return (
+      <tr key={index}>
+      <a href={module.url}> {module.name} </a>
+      <a href={module.url}> {module.noLectures}</a>
+      <a href={module.url}>{module.noPracticals}</a>
+      
+      </tr>
+    
+    );
+  });
   return (
-      <h1>TODO</h1>
+    <>
+      <h2>{props.course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <h2>{props.type}</h2>
+      
+        <tbody>{list}</tbody>
+      
+      </table>
+    </>
   );
 };
 
